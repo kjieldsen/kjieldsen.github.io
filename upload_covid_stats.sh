7e19c45d79234eec09810099f99fd4deb69d6d40
kjieldsen <kjieldsen@gmail.com>
2021-01-26 22:57:16 -0800

    update covid stats

--- upload_covid_stats.sh
+++ upload_covid_stats.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 csplit -f table /Users/justinleung/github/kjieldsen.github.io/covid_datatables.html "/<table /" {2}
-mv table00 table00.html
-mv table01 table01.html
-mv table02 table02.html
-mv table03 table03.html
+
+paste table_css.html table00 > table00.html
+paste table_css.html table01 > table01.html
+paste table_css.html table02 > table02.html
+paste table_css.html table03 > table03.html
 
 git add --all
 git commit -m "update covid stats"
