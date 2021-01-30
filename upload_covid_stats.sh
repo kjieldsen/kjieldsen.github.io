a56eeec67c8145c7ffedaa2b08149e99920fb5d8
kjieldsen <kjieldsen@gmail.com>
2021-01-30 00:35:10 -0800

    update covid stats

--- upload_covid_stats.sh
+++ upload_covid_stats.sh
@@ -6,6 +6,7 @@ cat table_css.html table00 > table00.html
 cat table_css.html table01 > table01.html
 cat table_css.html table02 > table02.html
 cat table_css.html table03 > table03.html
+cat table_css.html table04 > table04.html
 
 git add --all
 git commit -m "update covid stats"
