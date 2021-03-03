8204b582babf4380abd9823ed011d68d26b81293
kjieldsen <kjieldsen@gmail.com>
2021-03-02 20:14:20 -0800

    update covid stats

--- upload_covid_stats.sh
+++ upload_covid_stats.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-csplit -f table /Users/justinleung/github/kjieldsen.github.io/covid_datatables.html "/<table /" {4}
+csplit -f table /Users/justinleung/github/kjieldsen.github.io/covid_datatables.html "/<table /" {6}
 
 cat table_css.html table00 > table00.html
 cat table_css.html table01 > table01.html
@@ -9,6 +9,7 @@ cat table_css.html table03 > table03.html
 cat table_css.html table04 > table04.html
 cat table_css.html table05 > table05.html
 cat table_css.html table06 > table06.html
+cat table_css.html table07 > table07.html
 
 ls -ltr
 
